f46cd3262b0ba0e7ed15f07471b53a65f7b36a1c
Nestor Rosales <rosales.nestorr@gmail.com>
2024-02-27 19:57:42 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,ts}"],
+	theme: {
+		extend: {},
+		colors: {
+			primary: "hsl(35, 77%, 62%)",
+			secondary: "hsl(5, 85%, 63%)",
+			"very-dark-blue": "hsl(240, 100%, 5%)",
+			"dark-grayish-blue": "hsl(236, 13%, 42%)",
+			"light-grayish-blue": "hsl(240, 6%, 75%)",
+		},
+	},
+	plugins: [],
+};
