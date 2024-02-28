922d8a3e68d8c460c3e4a800bb8e0e8d7aee9378
Nestor Rosales <rosales.nestorr@gmail.com>
2024-02-28 18:11:40 -0300

    resized elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@
 module.exports = {
 	content: ["./src/**/*.{html,ts}"],
 	theme: {
-		extend: {},
-		colors: {
-			primary: "hsl(35, 77%, 62%)",
-			secondary: "hsl(5, 85%, 63%)",
-			"very-dark-blue": "hsl(240, 100%, 5%)",
-			"dark-grayish-blue": "hsl(236, 13%, 42%)",
-			"light-grayish-blue": "hsl(240, 6%, 75%)",
+		extend: {
+			colors: {
+				primary: "hsl(35, 77%, 62%)",
+				secondary: "hsl(5, 85%, 63%)",
+				"very-dark-blue": "hsl(240, 100%, 5%)",
+				"dark-grayish-blue": "hsl(236, 13%, 42%)",
+				"light-grayish-blue": "hsl(240, 6%, 75%)",
+			},
 		},
 	},
 	plugins: [],
