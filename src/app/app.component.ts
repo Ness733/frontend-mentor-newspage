0a84273070f00df7722bcfa0d8ecab55302990bf
Nestor Rosales <rosales.nestorr@gmail.com>
2024-03-01 23:31:44 -0300

    news component refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, NO_ERRORS_SCHEMA } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { NewsItemComponent } from "./news-item/news-item.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: "app-root",
+	standalone: true,
+	imports: [RouterOutlet, NewsItemComponent],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'frontend-challenge-newspage';
+	title = "frontend-challenge-newspage";
 }
